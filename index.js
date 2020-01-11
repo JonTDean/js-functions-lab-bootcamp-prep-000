a3d3c20320995154c05325b3dee473eaba4eef90
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return `Happy holiday, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name){
-  return `happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, countdown){
