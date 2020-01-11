6ad8eed781996da4c5b270cb63a73a30b549ac59
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:30:47 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -24,7 +24,7 @@ describe('functions', () => {
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Halloween", 20)).toEqual("It's 20 days until Halloween!")
-      expect(holidayCountdown("Father's Day", 120)).toEqual("It's 120 days until Father's Day!")
+      expect(holidayCountdown("Halloween", 120)).toEqual("It's 120 days until Halloween")
     })
   })
 })
