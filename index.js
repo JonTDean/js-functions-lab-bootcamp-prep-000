c2af47dae0028c6287d35f03706e14f5b5238935
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:26:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+var name = "Jonathan"
+var holiday = "Halloween"
+var days = 20
+
+function happyHolidays(){
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name){
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, countdown){
+  return `It's ${days} days until ${holiday}!`
+}
